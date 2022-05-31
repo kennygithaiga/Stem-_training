2489d422de3e1c3e1e84e81d6fe27df50b555f91
kennygithaiga <105698741+kennygithaiga@users.noreply.github.com>
2022-05-31 01:55:52 -0700

    Add files via upload

---
+++ instances.py
@@ -0,0 +1,7 @@
+from school import School
+
+
+school_one=School(414, "Nairobi")
+school_two=School(562,"Mombasa")
+school_three=School(123,"Kisumu")
+print(school_one.no_of_students,school_one.location)
---
+++ store.py
@@ -0,0 +1,9 @@
+from library import books
+
+ 
+def remainder ():
+    print("there are four bales remaining")
+books()
+
+from store import remainder
+remainder()
